1f60fc8652b0f4135a574c765745fcec415dcd3c
AihamAR <aihama@copyleaks.com>
2022-12-08 09:09:59 +0200

    add tags property to results model

---
+++ CopyleaksAPI/Models/Responses/Result/TagsResult.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+
+namespace Copyleaks.SDK.V3.API.Models.Responses.Result
+{
+	public class TagsResult
+	{
+		[JsonProperty("code")]
+		public string Code { get; set; }
+
+		[JsonProperty("title")]
+		public string Title { get; set; }
+
+		[JsonProperty("description")]
+		public string Description { get; set; }
+	}
+}
