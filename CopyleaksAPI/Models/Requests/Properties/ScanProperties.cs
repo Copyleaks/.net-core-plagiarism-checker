475db28765c1c25c0fdb46c4fde653e34b3b7384
Elazarcl <elazarb@copyleaks.com>
2023-06-08 09:31:35 +0300

    remove properties.priority

--- CopyleaksAPI/Models/Requests/Properties/ScanProperties.cs
+++ CopyleaksAPI/Models/Requests/Properties/ScanProperties.cs
@@ -54,12 +54,6 @@ namespace Copyleaks.SDK.V3.API.Models.Requests.Properties
 		public string DeveloperPayload { get; set; }
 
 		/// <summary>
-		/// Scan priority
-		/// </summary>
-		[JsonProperty("priority")]
-		public eScanPriority Priority { get; set; } = eScanPriority.Normal;
-
-		/// <summary>
 		/// Enable sandbox scan.
 		/// </summary>
 		[JsonProperty("sandbox")]
